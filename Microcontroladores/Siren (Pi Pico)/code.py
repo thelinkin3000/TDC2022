fc2a2988ab46e370b30f4888c0daec085582e538
Carlos Pérez <carlos.g.perez.1992@outlook.com>
2022-08-22 20:09:25 -0300

    Fixed a bug on Siren (Pi Pico)

--- Microcontroladores/Siren (Pi Pico)/code.py
+++ Microcontroladores/Siren (Pi Pico)/code.py
@@ -2,18 +2,19 @@ import time
 import board
 import pwmio
 
-# Configuramos el pin de salida LED en modo PWM, con una 
-# frecuencia de 5MHz y un duty cycle de 0, es decir apagado todo el tiempo:
+# Configuramos el pin de salida LED en modo PWM, con una
+# frecuencia de 5MHz y porcentaje de duty cycle activo de 0,
+# es decir apagado todo el tiempo:
 led = pwmio.PWMOut(board.LED, frequency=5000, duty_cycle=0)
 
 while True:
     for i in range(100):
         if i < 50:
-            # En la primer mitad del ciclo de 100 pasos vamos aumentando 
+            # En la primer mitad del ciclo de 100 pasos vamos aumentando
             # el duty cycle paulatinamente hasta llegar al máximo de 65535
-            led.duty_cycle = int(i * 2 * 65535 / 100)  # Up
+            led.duty_cycle = int(i * 2 * 65535 / 100)
         else:
-            # En la segunda mitad del ciclo hacemos el proceso inverso, 
+            # En la segunda mitad del ciclo hacemos el proceso inverso,
             # disminuyendo hasta llegar a 0
-            led.duty_cycle = 65535 - int((i - 50) * 2 * 65535 / 100)  # Down
+            led.duty_cycle = 65535 - int((i - 50) * 2 * 65535 / 100)
         time.sleep(0.01)
